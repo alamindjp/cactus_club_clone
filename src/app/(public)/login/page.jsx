d883e80c286d6ac9a6a7f0b9774c00422df41946
alamindjp <alaminhossaindjp@gmail.com>
2024-01-09 22:07:34 +0600

    resposive log in page

--- src/app/(public)/login/page.jsx
+++ src/app/(public)/login/page.jsx
@@ -1,35 +1,29 @@
-import Image from 'next/image';
-import bg_img from '../../../assets/image/login_bg.png';
-import LoginHeader from './LoginHeader';
-import InputField from './InputField';
+import Image from "next/image";
+import bg_img from "@/assets/image/login_bg.png";
+import bg_img_mobile from "@/assets/image/login_mobile.png";
+import LoginHeader from "./LoginHeader";
+import InputField from "./InputField";
+import Link from "next/link";
+import Button from "@/app/components/Button/Button";
 
 const page = () => {
-
-    
-    return (
-        <div className='login_bg'>
-            <Image src={bg_img} alt="" />
-            <div className='login'>
-                <LoginHeader/>
-                <h1 className='login_welcome'>Welcome to the Cactus club!</h1>
-                <div className='input_wrapper'>
-                <InputField 
-                key="username"
-                name="username"
-                type="text"
-                />
-                <InputField 
-                key="password"
-                name="password"
-                type="password"
-                />
-                </div>
-           
-
-            </div>
-            
+  return (
+    <div className="login_bg">
+      <Image src={bg_img} className="hidden sm:block" alt="bg_image"/>
+      <Image src={bg_img_mobile} className="block sm:hidden" alt="bg_image"/>
+      <div className="login">
+        <LoginHeader />
+        <h1 className="login_welcome">Welcome to the Cactus club!</h1>
+        <div className="input_wrapper">
+          <InputField key="username" name="username" type="text" />
+          <InputField key="password" name="password" type="password" />
         </div>
-    );
+        <p className="forget_pass"><Link href="/">forget password</Link></p>
+        <Button label="login"/>
+        <p className="create_account">do not have an account? <Link href="/">signup</Link></p>
+      </div>
+    </div>
+  );
 };
 
 export default page;
